6cd9f980a311c691ceedb3ae76a1e532aef977dd
Gregory Bilello <email": null>
2016-11-23 16:17:42 -0500

    Working on CSS for sidebar.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,14 @@
 class SessionsController < ApplicationController
 
-  def index
+	def index
+	end
+
+  def create
+  end
+
+  def destroy
+  	session.clear
+  	redirect_to 'sessions#index'
   end
 
 end
