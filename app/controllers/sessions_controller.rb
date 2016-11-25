1615b40c3dd724dc2e2ff7f819807c6f24caaf61
Gregory V. Bilello <gregory.bilello@gmail.com>
2016-11-25 11:17:07 -0500

    Working on log out button.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,11 @@
 class SessionsController < ApplicationController
 
 	def index
+		# redirect if user is already logged in
 		if session[:auth]
 			redirect_to users_index_path
 		end
 	end
-
-  def create
-  end
 
   def destroy
   	session.clear
