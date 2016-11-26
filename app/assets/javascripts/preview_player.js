565579fcb9186948e07c8ec39dee519830f314ef
regulus33 <zacharyrowden89@gmail.com>
2016-11-25 22:02:16 -0500

    Add graphics partial and sample player partial, write jquery to dynamically update audio tag src attribute.

--- app/assets/javascripts/preview_player.js
+++ app/assets/javascripts/preview_player.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
 
-	$(document).on("click",".user-playlist",function (event){ 
+	$(document).on("click",".user-playlist", function (event){ 
 	     event.preventDefault(); 
 	     $("div.user-spotify-player").remove();
 	     $.ajax({
@@ -13,8 +13,34 @@ $( document ).ready(function() {
 	     })
 	 
 	});
+
+
+	 $(document).on("click",".track",function(event){ 
+	 	event.preventDefault();
+        $("audio").attr({"src" : $(this).attr("id")});
+        // audioSetup();
+        // visualSetup();
+        // draw();
+      }); 
   
 });
+// $( document ).ready(function() {
+
+// 	$(".user-playlist").click(function (event){ 
+// 	     event.preventDefault(); 
+// 	     $("div.user-spotify-player").remove();
+// 	     $.ajax({
+// 	        url: '/playlist',
+// 	        method: 'get',
+// 	        data: {index : $(this).attr('id')}
+// 	     }).done(function(response){
+// 	     	console.log(response)
+// 	     	$("div.sidebar").append(response)
+// 	     })
+	 
+// 	});
+  
+// });
 
 
 
