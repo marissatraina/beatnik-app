42981ea936106136a0ed93c200120d5cd6a6727f
marissatraina <marissatraina@gmail.com>
2016-11-26 21:18:25 -0800

    mark resolution in visual container

--- app/assets/javascripts/player.js
+++ app/assets/javascripts/player.js
@@ -7,11 +7,13 @@ $(document).ready(function() {
 		var playlistLink = "'https://embed.spotify.com/?uri=" + playlistUri + "'";
 		var iframePlaylist = "<iframe src=" + playlistLink + " width='300' height='380' frameborder='0' allowtransparency='true'></iframe>";
 		$(".user-spotify-player").html(iframePlaylist);
-	})
+	});
 
-	$("button#side-hide").toggle(function(){
-		$('div#visual-container').css('left', '0');
-	}, function() {
-		$('div#visual-container').css('left', '305px')
-	})
-})
+	var $visual = $("#visual-container");
+	$("button#side-button").click(function() {
+		$(this).html(function(i, html){
+			return html === "&#60;" ? "&#x2630;" : "&#60;";
+		});
+		$visual.toggleClass("full");
+	});
+});
