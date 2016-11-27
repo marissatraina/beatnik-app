7ed26d9088f1dbe0fcb0311534c34f682579c276
marissatraina <marissatraina@gmail.com>
2016-11-27 14:00:41 -0800

    Complete collapsable sidebar styling and functionality

--- app/assets/javascripts/player.js
+++ app/assets/javascripts/player.js
@@ -12,9 +12,9 @@ $(document).ready(function() {
 	var $visual = $("#visual-container");
 	$("button#side-button").click(function() {
 			if ($visual.hasClass("full")) {
-				$(this).html("&#x2630;");
-			} else {
 				$(this).html("&#60;");
+			} else {
+				$(this).html("&#x2630;");
 			};
 		$visual.toggleClass("full");
 	});
