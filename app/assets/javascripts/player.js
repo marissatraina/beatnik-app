52f4030a27f815def74044f7d63547fc295d02a4
Gregory Bilello <email": null>
2016-11-28 12:18:08 -0500

    Refactor html and add functioning sidebar with canvas full screen

--- app/assets/javascripts/player.js
+++ app/assets/javascripts/player.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
 
-	var $visual = $("#visual-container");
+	var $hidden = $("#hidden-container");
 	$("button#side-button").click(function() {
-			if ($visual.hasClass("full")) {
+			if ($hidden.hasClass("hidden")) {
 				$(this).html("&#60;");
 			} else {
 				$(this).html("&#x2630;");
 			};
-		$visual.toggleClass("full");
+		$hidden.toggleClass("hidden");
+		$(this).toggleClass("forward");
 	});
-	
 });
