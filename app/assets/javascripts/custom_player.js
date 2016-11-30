858f46e4fd9b802105fbabc36f2337001117fcf8
marissatraina <marissatraina@gmail.com>
2016-11-30 18:41:30 -0500

    Fix audio ajax

--- app/assets/javascripts/custom_player.js
+++ app/assets/javascripts/custom_player.js
@@ -1,38 +1,37 @@
 $( document ).ready(function() {
 
-	$(document).on("click",".user-playlist", function (event){ 
-	     event.preventDefault(); 
-	     playlist_name = $(this).attr("data-playlist-name")
-	     first_track_img = $(this).attr("data-first-trck-img")
-	     console.log("IMAGE", first_track_img)
-			 $(".mejs-pause button").css("background-position", "0 0" )	     
-			 $("h2").text(playlist_name);
-	     $("#player-art").attr({"src" : first_track_img})
-			 $("audio").attr({"src" : $("li.track").first().parent().attr("id")})
-			 $("li.track").remove();
+	$(document).on("click", "div.user-playlists td.user-playlist", function (event){
+	     event.preventDefault();
+		     playlist_name = $(this).attr("data-playlist-name")
+		     first_track_img = $(this).attr("data-first-trck-img")
+				 $(".mejs-pause button").css("background-position", "0 0" )
+				 $("h2#playlist-name").text(playlist_name);
+		     $("#player-art").attr({"src" : first_track_img})
+				 $("audio").attr({"src" : $("li.track").first().parent().attr("id")})
+				 $("ul#playlist a").remove();
 	     $.ajax({
 	        url: '/playlist',
 	        method: 'get',
 	        data: {index : $(this).attr('id')}
 	     }).done(function(response){
-	     	$("ul#playlist").append(response);
-	     	$("h2#playing-artist").text($("ul").children().first().attr("data-artist-name"))
-	      $("h2#playing-song").text($("ul").children().first().attr("data-track-name"))
+		     	$("ul#playlist").append(response);
+		     	$("h2#playing-artist").text($("ul#playlist a").first().attr("data-artist-name"));
+		      $("h2#playing-song").text($("ul#playlist a").first().attr("data-track-name"));
 
 	     	renderPlayer();
 	     })
-	 
+
 	});
 
 
-	 $(document).on("click",".track",function(event){ 
-	 	event.preventDefault();	
+	 $(document).on("click",".track",function(event){
+	 	event.preventDefault();
 	      $("audio").attr({"src" : $(this).attr("id")});
 	      $("#player-art").attr({"src" : $(this).attr("data-img-url")})
 	      $("h2#playing-artist").text($(this).attr("data-artist-name"))
 	      $("h2#playing-song").text($(this).attr("data-track-name"))
 	      $("button").eq(1).click()
-	
+
 	    });
 
 	 $(document).on("click", $("button").eq(1), function(){
